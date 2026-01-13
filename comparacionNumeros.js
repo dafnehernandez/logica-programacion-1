ec844e08f417fa99e11b2996fdbe6bdd1e29fda5
Dafne Hernandez <mcflurrycerezita@gmail.com>
2026-01-13 16:40:16 -0600

    feat: Estructura esqueleto para el ejercicio

---
+++ comparacionNumeros.js
@@ -0,0 +1,14 @@
+// Solicitar primer número
+let numero1 = parseInt(prompt("Ingresa el primer número:"));
+
+// Solicitar segundo número
+let numero2 = parseInt(prompt("Ingresa el segundo número:"));
+
+// Solicitar tercer número
+let numero3 = parseInt(prompt("Ingresa el tercer número:"));
+
+// Mostrar los valores guardados en la consola
+console.log("Número 1:", numero1);
+console.log("Número 2:", numero2);
+console.log("Número 3:", numero3);
+
